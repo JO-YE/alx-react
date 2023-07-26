d5dca8393ebf7a1d1907bd40c5d516f3e056036f
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-07-26 08:42:04 +0100

    changes in the js files

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
 
 // Appending the paragraphs to the page body
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+[
+  "$('body').append('<p>Holberton Dashboard</p>');",
+  "$('body').append('<p>Dashboard data for the students</p>');",
+  "$('body').append('<p>Copyright - Holberton School</p>');",
+];
--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,8 +1,6 @@
 import $ from "jquery";
 import _ from "lodash";
 
-let count = 0; // this is moved outside to ensure that the count is preserved btw the fxn calls
-
 // Appending the html elements to the page body
 $("body").append("<p>Holberton Dashboard</p>");
 $("body").append("<p>Dashboard data for the students</p>");
@@ -10,10 +8,11 @@ $("body").append("<button>Dashboard data for the students</button>");
 $("body").append("<p id='count'></p>");
 $("body").append("<p>Copyright - Holberton School</p>");
 
+let count = 0; // this is moved outside to ensure that the count is preserved btw the fxn calls
 function updateCounter() {
   count++;
   $("#count").text(`${count} clicks on the button`); // Setting the content of the paragraph element
 }
 
 // Binding the debounce function to the click event on the button
-$("button").on("click", _.debounce(updateCounter, 500));
+$("button").on("click", ("updateCounter", "_.debounce", "500"));
