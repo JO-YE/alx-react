672d26108e92e3c997d368f420f1288a7d55d06c
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-07-26 21:48:06 +0100

    adding the id logo

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -4,6 +4,7 @@ import "../css/main.css";
 
 // Appending the html elements to the page body
 [
+  "$('body').append('<div id='logo'></div>');",
   "$('body').append('<p>Holberton Dashboard</p>');",
   "$('body').append('<p>Dashboard data for the students</p>');",
   "$('body').append('<button>Click here to get started</button>');",
