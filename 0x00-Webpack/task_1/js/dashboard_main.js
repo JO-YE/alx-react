edf76ac1cadb899484b0aa74c8ed16f3e8ee93cb
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-07-26 06:37:02 +0100

    appending the html element to the body

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,20 +2,18 @@ import $ from "jquery";
 import debounce from "lodash/debounce";
 
 let count = 0; // this is moved outside to ensure that the count is preserved btw the fxn calls
-// Creating html elements
-const p1 = $("<p>").text("Holberton Dashboard");
-const p2 = $("<p>").text("Dashboard data for the students");
-const button = $("<button>").text("Click here to get started");
-const p3 = $("<p>").attr("id", "count");
-const p4 = $("<p>").text("Copyright - Holberton School");
 
-// Appending the paragraphs to the page body
-$("body").append(p1, p2, button, p3, p4);
+// Appending the html elements to the page body
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Dashboard data for the students</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
 
 function updateCounter() {
-  count++;
+  $("button").on("click", () => count++);
   $("#count").text(`${count} clicks on the button`); // Setting the content of the paragraph element
 }
 
 // Binding the debounce function to the click event on the button
-button.on("click", debounce(updateCounter, 500));
+debounce(updateCounter, 500);
