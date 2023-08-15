6d409ce170f6d2ad74096cb404f580f286ff633d
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-08-15 16:49:24 +0100

    updated

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,26 +1,17 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
 describe("getFullYear", () => {
-  it("returns the current year", () => {
-    const currentYear = new Date().getFullYear();
-    expect(getFullYear()).toEqual(currentYear);
-  });
+  const currentYear = new Date().getFullYear();
+  expect(getFullYear()).toEqual(currentYear);
 });
 
 describe("getFooterCopy", () => {
-  it("returns the correct string when isIndex is true", () => {
-    expect(getFooterCopy(true)).toEqual("Holberton School");
-  });
-
-  it("returns the correct string when isIndex is false", () => {
-    expect(getFooterCopy(false)).toEqual("Holberton School main dashboard");
-  });
+  expect(getFooterCopy(true)).toEqual("Holberton School");
+  expect(getFooterCopy(false)).toEqual("Holberton School main dashboard");
 });
 
 describe("getLatestNotification", () => {
-  it("returns the correct string", () => {
-    expect(getLatestNotification()).toEqual(
-      "<strong>Urgent requirement</strong> - complete by EOD"
-    );
-  });
+  expect(getLatestNotification()).toBe(
+    "<strong>Urgent requirement</strong> - complete by EOD"
+  );
 });
